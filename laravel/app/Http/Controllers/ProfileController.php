c416cccb8a59268c367a14a12d351f15955bc569
Mengleang30 <novmengleang14@gmail.com>
2025-03-12 13:40:11 +0700

    create TP06

--- laravel/app/Http/Controllers/ProfileController.php
+++ laravel/app/Http/Controllers/ProfileController.php
@@ -52,8 +52,11 @@ class ProfileController extends Controller
 
         $user->delete();
 
+
         $request->session()->invalidate();
+        
         $request->session()->regenerateToken();
+
 
         return Redirect::to('/');
     }
